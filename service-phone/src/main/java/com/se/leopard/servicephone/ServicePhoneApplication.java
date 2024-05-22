bfb3557966e55c1bf78c21a68fd4790f698409c1
thienngon1911 <ngonshadow0503@gmail.com>
2024-05-22 23:13:35 +0700

    khoi tao service-phone crud

---
+++ service-phone/src/main/java/com/se/leopard/servicephone/ServicePhoneApplication.java
@@ -0,0 +1,13 @@
+package com.se.leopard.servicephone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicePhoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicePhoneApplication.class, args);
+	}
+
+}
---
+++ service-phone/src/test/java/com/se/leopard/servicephone/ServicePhoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se.leopard.servicephone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicePhoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
