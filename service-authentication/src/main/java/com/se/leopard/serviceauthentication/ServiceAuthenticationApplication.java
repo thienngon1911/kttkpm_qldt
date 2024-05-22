1ecc9a50eab560b75efa5f7f02cdad2ed4895fb7
VanChanh091 <vanchanh0730@gmail.com>
2024-05-22 23:24:05 +0700

    tao project microservice authentication

---
+++ service-authentication/src/main/java/com/se/leopard/serviceauthentication/ServiceAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.se.leopard.serviceauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceAuthenticationApplication.class, args);
+	}
+
+}
---
+++ service-authentication/src/test/java/com/se/leopard/serviceauthentication/ServiceAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se.leopard.serviceauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
