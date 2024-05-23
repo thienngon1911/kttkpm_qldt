7b72bd758497bc3400e9e39ce439c4e74b5fccea
VanChanh091 <vanchanh0730@gmail.com>
2024-05-23 09:06:47 +0700

    khoi tao service-api gateway & discovery server

---
+++ service-apigateway/src/main/java/com/se/leopard/serviceapigateway/ServiceApigatewayApplication.java
@@ -0,0 +1,13 @@
+package com.se.leopard.serviceapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceApigatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceApigatewayApplication.class, args);
+	}
+
+}
---
+++ service-apigateway/src/test/java/com/se/leopard/serviceapigateway/ServiceApigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se.leopard.serviceapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApigatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
