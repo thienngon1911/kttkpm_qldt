ed139233e63c9fa0f4a8a423798e78e744d70872
thienngon1911 <ngonshadow0503@gmail.com>
2024-05-24 13:56:29 +0700

    resilience4j - circuitbreaker, retry, ratelimiter, timelimiter trong resttemplate

--- rest-template-resilience4j/src/main/java/com/se/leopard/rest_template_resilience4j/RestTemplateResilience4jApplication.java
+++ rest-template-resilience4j/src/main/java/com/se/leopard/rest_template_resilience4j/RestTemplateResilience4jApplication.java
@@ -7,9 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class RestTemplateResilience4jApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RestTemplateResilience4jApplication.class, args);
 	}
-
 }
--- rest-template-resilience4j/src/main/java/com/se/leopard/rest_template_resilience4j/controller/RestController.java
+++ rest-template-resilience4j/src/main/java/com/se/leopard/rest_template_resilience4j/controller/RestController.java
@@ -1,6 +1,13 @@
 package com.se.leopard.rest_template_resilience4j.controller;
 
+import java.net.ConnectException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
@@ -15,11 +22,17 @@ import org.springframework.web.client.RestTemplate;
 
 import com.se.leopard.rest_template_resilience4j.entity.Phone;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
+
 @org.springframework.web.bind.annotation.RestController
 @RequestMapping("/rest-template")
 public class RestController {
 	
 	private final String SERVICE_PHONE = "http://localhost:8082/phone";
+	private int ATTEMP_RETRY = 1;
 	
 	@Autowired
 	private RestTemplate restTemplate;
@@ -30,7 +43,11 @@ public class RestController {
 	}
 	
 	@GetMapping("/getPhones")
+//	@CircuitBreaker(name = "CIRCUIT_BREAKER_1", fallbackMethod = "fallbackCircuitGetPhones")
+//	@Retry(name = "RETRY_1", fallbackMethod = "fallbackRetryGetPhones")
+//	@RateLimiter(name = "RATE_LIMITER_1", fallbackMethod = "fallbackRateLimiterGetPhones")
 	public List<Phone> getPhones(@RequestHeader("Authorization") String authorizationHeader) {
+//		System.out.println("\n\nMethod findAllPlant() retry " + ATTEMP_RETRY++ + " times at " + LocalDateTime.now() + "\n\n"); // retry
 		HttpHeaders headers = new HttpHeaders();
         headers.set("Authorization", authorizationHeader);
         
@@ -42,4 +59,54 @@ public class RestController {
 			);
 		return responseEntity.getBody();
 	}
+	
+	@GetMapping("/simulateTimeLimiter")
+	@TimeLimiter(name = "TIME_LIMITER_1", fallbackMethod = "fallbackTimeLimiter")
+	public CompletableFuture<List<Phone>> testTimeLimiter(@RequestHeader("Authorization") String authorizationHeader) {
+		return CompletableFuture.supplyAsync(() -> {
+            try {
+                HttpHeaders headers = new HttpHeaders();
+                headers.set("Authorization", authorizationHeader);
+
+                // Giả lập độ trễ 3 giây
+                TimeUnit.SECONDS.sleep(3);
+
+                ResponseEntity<List<Phone>> responseEntity = restTemplate.exchange(
+                        SERVICE_PHONE + "/list",
+                        HttpMethod.GET,
+                        new HttpEntity<>(headers),
+                        new ParameterizedTypeReference<List<Phone>>() {}
+                );
+
+                return responseEntity.getBody();
+            } catch (Exception e) {
+                e.printStackTrace();
+                throw new RuntimeException(e);
+            }
+        });
+	}
+
+	// FALLBACK METHODS
+	public List<Phone> fallbackCircuitGetPhones(String authorizationHeader, Throwable throwable) {
+		return Stream.of(
+				new Phone(0, "Circuit Breaker", "Make in trouble in", 0, 0, 0, 0, "The microservice you call is now dead.", "Circuit breaker is now open.", 0, 0, 0)
+		).collect(Collectors.toList());
+    }
+	
+	public List<Phone> fallbackRetryGetPhones(String authorizationHeader, Throwable throwable) {
+		return Stream.of(
+				new Phone(0, "Retry", "Retry", 0, 0, 0, 0, "The microservice " + SERVICE_PHONE + " was not response: " + ATTEMP_RETRY + " times", "Retry Exception.", 0, 0, 0)
+		).collect(Collectors.toList());
+    }
+	
+	public List<Phone> fallbackRateLimiterGetPhones(String authorizationHeader, Throwable throwable) {
+		return Stream.of(
+				new Phone(0, "Rate Limiter", "Please don't spam call data", 0, 0, 0, 0, "Too much requests.", "Please slow down!", 0, 0, 0)
+		).collect(Collectors.toList());
+    }
+	public CompletableFuture<List<Phone>> fallbackTimeLimiter(String authorizationHeader, Throwable throwable) {
+        return CompletableFuture.completedFuture(List.of(
+        		new Phone(0, "Time Limiter", "Sorry, server is now bussy!", 0, 0, 0, 0, "Your request took to long response.", "Please retry in minutes!", 0, 0, 0)
+        ));
+    }
 }
